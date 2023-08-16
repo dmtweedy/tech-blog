84dda05cfaa334f075dfd25aee963597f25b7c41
dmtweedy <damon.tweedy@yahoo.com>
2023-08-16 14:59:14 -0500

    updated auth based on userid, in last commit also removed auth.js and partials.js

--- controllers/dashController.js
+++ controllers/dashController.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
+
 const { Post } = require('../models');
 
 // Middleware function to check if the user is authenticated
 function isAuthenticated(req, res, next) {
-  if (req.isAuthenticated()) {
+  if (req.session.userId) {
     return next();
   }
   res.redirect('/login'); // Redirect to login if not authenticated
